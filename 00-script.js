ddb891a886c4d41ee1cc7fe951d39e0ff9cf1c6c
Ismael24 <60234363+Ismael24@users.noreply.github.com>
2021-04-28 10:31:48 +0200

    toda la página hasta ahora 28/04/2021

---
+++ 00-script.js
@@ -0,0 +1,6 @@
+function overlayOn5() {
+    document.getElementById("overlaySerie").style.display = "block";
+}
+function overlayOff5() {
+    document.getElementById("overlaySerie").style.display = "none";
+}
